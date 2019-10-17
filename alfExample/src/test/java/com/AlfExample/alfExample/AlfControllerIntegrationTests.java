7e8ea984fdb801c05e291ea4d2b273ceafdf8fd8
Simona Carp <Simona.Carp@endava.com>
2019-10-17 13:29:24 +0300

    Step4: Fixed Integration tests

--- alfExample/src/test/java/com/AlfExample/alfExample/AlfControllerIntegrationTests.java
+++ alfExample/src/test/java/com/AlfExample/alfExample/AlfControllerIntegrationTests.java
@@ -3,51 +3,56 @@ package com.AlfExample.alfExample;
 import com.AlfExample.alfExample.controllers.AlfController;
 import com.AlfExample.alfExample.service.AlfService;
 import com.AlfExample.alfExample.service.AlfServiceImplementation;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
-public class AlfControllerTests {
+public class AlfControllerIntegrationTests {
     private MockMvc mockMvc;
     @Autowired
     protected WebApplicationContext wac;
-    @MockBean
-    private AlfService alfServiceMock;
-    @Autowired
-    private AlfController alfController;
-    String displayedText;
 
+    @Mock
+    private AlfService alfService;
+    @InjectMocks
+    private AlfController alfController;
 
     @Before
-    public void setup(){
+    public void setup() {
         alfController = new AlfController();
         this.mockMvc = standaloneSetup(this.alfController).build();
-        displayedText = "The list is: [1, 2, alfredo, 4, buzz]. Report: fizz 0, buzz 1, fizzbuzz 0, alfredo 1, integer 3";
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testDisplayOutputWithANumberVariable() throws Exception {
+        this.alfService = spy(AlfService.class);
+        mockMvc.perform(get("/display/{number}", 5)).andDo(print())
+                .andExpect(status().isOk()).andReturn();
     }
     @Test
-    public void testDisplayOutput() throws Exception {
-   /* when(alfServiceMock.printFizzBuzz(any(Integer.class))).thenReturn(displayedText);
-    mockMvc.perform(get("/display/{number}", 5)).andExpect(status().isOk());*/
+    public void testDisplayOutputWithAStringVariable() throws Exception {
+        this.alfService = spy(AlfService.class);
+        MvcResult result = mockMvc.perform(get("/display/{number}", "String")).andDo(print())
+                .andExpect(status().is4xxClientError()).andReturn();
     }
 }
